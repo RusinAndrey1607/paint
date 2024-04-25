63c59ea57d5932cef50539e834655a446eea75e9
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-25 08:33:16 +0300

    Edit tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -2,11 +2,7 @@ import type { Config } from "tailwindcss";
 import daisyui from 'daisyui'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
