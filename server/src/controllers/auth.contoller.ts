df45ae2247667b15405f9314a437335e9599844b
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-28 13:54:01 +0300

    Add auth controller

---
+++ server/src/controllers/auth.contoller.ts
@@ -0,0 +1,17 @@
+class AuthController {
+    async registration(req: Request, res: Response, next: Function) {
+    }
+    async login(req: Request, res: Response, next: Function) {
+
+    }
+    async logout(req: Request, res: Response, next: Function) {
+        
+    }
+    async refresh(req: Request, res: Response, next: Function) {
+       
+    }
+    async getAll(req: Request, res: Response, next: Function) {
+    }
+}
+
+export const authController = new AuthController()
