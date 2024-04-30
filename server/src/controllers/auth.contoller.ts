d70cb1f270eed1c4b369cf669360ef7ed32d3c9c
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-04-30 09:44:17 +0300

    Add registration to contoller

--- server/src/controllers/auth.contoller.ts
+++ server/src/controllers/auth.contoller.ts
@@ -1,5 +1,18 @@
+import { Request, Response } from "express"
+import { authService } from "../services/auth.service"
+
 class AuthController {
     async registration(req: Request, res: Response, next: Function) {
+        try {
+            const { email, password } = req.body
+
+            const userData = await authService.registration(email, password)
+            
+            res.cookie("refreshToken", userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true })
+            return res.json(userData)
+        } catch (error) {
+            next(error)
+        }
     }
     async login(req: Request, res: Response, next: Function) {
     }
