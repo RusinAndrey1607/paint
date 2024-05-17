aa5ad69f2b6b85f8684bdb5dbdf0b5af1fa07219
RusinAndrey1607 <66871844+RusinAndrey1607@users.noreply.github.com>
2024-05-17 21:07:52 +0300

    Fix auth services

--- client/src/entities/Auth/model/services/loginByEmailAndPassword/loginByEmailAndPassword.ts
+++ client/src/entities/Auth/model/services/loginByEmailAndPassword/loginByEmailAndPassword.ts
@@ -1,12 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { api, models } from "shared/api/paint";
+import { paintApi } from "shared/api/index";
 import { ACCESS_TOKEN_LOCALSTORAGE_KEY } from "shared/consts/localStorage";
 
 const loginByEmailAndPassword = createAsyncThunk(
     'auth/loginByEmailAndPassword',
-    async (authData: models.IAuthRequest, thunkAPI) => {
+    async (authData: paintApi.models.IAuthRequest, thunkAPI) => {
       try {
-        const {data} = await api.$login(authData);
+        const {data} = await paintApi.api.$login(authData);
         localStorage.setItem(ACCESS_TOKEN_LOCALSTORAGE_KEY, data.accessToken)
         return data;
       } catch (error) {
--- client/src/entities/Auth/model/services/registerByEmailAndPassword/registerByEmailAndPassword.ts
+++ client/src/entities/Auth/model/services/registerByEmailAndPassword/registerByEmailAndPassword.ts
@@ -1,13 +1,12 @@
-import { models } from "@/shared/api/paint";
-import { api } from "@/shared/api/paint/";
-import { ACCESS_TOKEN_LOCALSTORAGE_KEY } from "@/shared/consts/localStorage";
+import { paintApi } from 'shared/api/';
+import { ACCESS_TOKEN_LOCALSTORAGE_KEY } from "shared/consts/localStorage";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 const registerByEmailAndPassword = createAsyncThunk(
     'auth/registerByEmailAndPassword',
-    async (authData: models.IAuthRequest, thunkAPI) => {
+    async (authData: paintApi.models.IAuthRequest, thunkAPI) => {
       try {
-        const {data} = await api.$registration(authData);
+        const {data} = await paintApi.api.$registration(authData);
         localStorage.setItem(ACCESS_TOKEN_LOCALSTORAGE_KEY, data.accessToken)
         return data;
       } catch (error) {
